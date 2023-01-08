3439dbb24eaebd27e917e24960d613c34aef8201
Eyob94 <eyobsh123@gmail.com>
2023-01-08 18:44:01 +0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Cards from "./components/Card/Cards";
-import Info from "./components/Info/Info";
 import Navbar from "./components/Navbar/Navbar";
-import Search from "./components/Search/Search";
+
 function App() {
 	return (
-		<div className="App flex justify-center">
+		<div className="flex justify-center App">
 			<div className="w-full max-w-5xl">
 				<Navbar />
-				<Info />
-				<Search />
-				<Cards />
 			</div>
 		</div>
 	);
