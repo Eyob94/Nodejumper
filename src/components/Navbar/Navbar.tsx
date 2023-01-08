9fd5d1442f89e4a39b6851530e5a8ebaed109e1b
Eyob94 <eyobsh123@gmail.com>
2023-01-08 18:59:52 +0300

    Initial Commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,14 @@
 const Navbar = () => {
-	return <div className="mb-4">h</div>;
+	return (
+		<div className="flex items-center justify-between h-40 mb-4 bg-white">
+			<div className="flex items-center title">
+				<div></div>
+				<div className="uppercase ">
+					Dairy<span className="text-blue-500">Data</span>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
